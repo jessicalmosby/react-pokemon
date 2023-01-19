09d76f59e82c7f4e32dfe0fd6a5e9ba6531506ef
Akilah Powell <atpjun@gmail.com>
2023-01-19 14:47:55 -0800

    loaded our pokemon and set our state via the hooks usePokemon.js and started on mapping in main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
+// import { usePokemon } from '../../hooks/usePokemon';
 
 export default function Main() {
-  return <main>Hello World</main>;
+  return <main></main>;
 }
---
+++ src/hooks/usePokemon.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+import { fetchInitialPokemon } from '../services/fetchPokemon';
+
+export function usePokemon() {
+  const [pokemon, setPokemon] = useState([]);
+  useEffect(() => { 
+    const fetchData = async () => {
+      const data = await fetchInitialPokemon();
+
+      setPokemon(data);
+    };
+    fetchData();
+  }, []);
+  return pokemon;
+}
