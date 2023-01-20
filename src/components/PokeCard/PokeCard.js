9f80cdfdce798b35a0b682ea44a76c8f08348686
Akilah Powell <atpjun@gmail.com>
2023-01-20 12:28:56 -0800

    got images to display

--- src/components/PokeCard/PokeCard.js
+++ src/components/PokeCard/PokeCard.js
@@ -5,9 +5,11 @@ export default function PokeCard({ pokemon, loading }) {
   return (
     <div className="poke-card">
       {pokemon.map((poke) => (
-        <p key={poke._id}>
+        <><p key={poke._id}>
           {poke.pokemon} {poke.type_1} {poke.type_2}
-        </p>
+        </p><img src={poke.url_image} width="200" height="200"
+          alt={poke.pokemon}/>
+        </>
       ))}
     </div>
   );
