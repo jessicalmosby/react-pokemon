5624f0ca6ea0da928c6417142b14f795a06eb420
Akilah Powell <atpjun@gmail.com>
2023-01-19 16:50:34 -0800

    created fetch type and fetch type{type} in services

--- src/services/fetchPokemon.js
+++ src/services/fetchPokemon.js
@@ -3,3 +3,15 @@ export async function fetchInitialPokemon() {
   const data = await resp.json();
   return data.results;
 }
+
+export async function fetchTypes() {
+  const resp = await fetch('https://alchemy-pokedex.herokuapp.com/api/pokedex/types');
+  const data = await resp.json();
+  return data;
+}
+
+export async function fetchPokemon(type) {
+  const resp = await fetch(`https://alchemy-pokedex.herokuapp.com/api/pokedex/types${type}`);
+  const data = await resp.json();
+  return data.results;
+}
